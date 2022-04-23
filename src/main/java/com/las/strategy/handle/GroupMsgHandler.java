9912194d4f5d66618751f22b2611d0b1d8a2ab79
dullwolf <2547170055@qq.com>
2022-04-24 00:54:57 +0800

    update

--- src/main/java/com/las/strategy/handle/GroupMsgHandler.java
+++ src/main/java/com/las/strategy/handle/GroupMsgHandler.java
@@ -1,6 +1,7 @@
 package com.las.strategy.handle;
 
 import com.las.strategy.BotMsgHandler;
+import com.las.utils.StrUtils;
 import org.apache.log4j.Logger;
 
 public class GroupMsgHandler extends BotMsgHandler {
@@ -10,7 +11,7 @@ public class GroupMsgHandler extends BotMsgHandler {
     @Override
     public void exec() {
         String msgData = getMsgData();
-        if(msgData.startsWith("#")){
+        if(StrUtils.isNotBlank(msgData) && msgData.startsWith("#")){
             logger.info("匹配到群消息前缀是#符号，开始执行指令");
             //执行指令
             exeCommand();
