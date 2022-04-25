96690b2c1ed17b295185468264bf88ca12fc8946
赵立田 <o_zhaolitian@cvte.com>
2022-04-25 16:10:45 +0800

    update

--- src/main/java/com/las/strategy/handle/FriendMsgHandler.java
+++ src/main/java/com/las/strategy/handle/FriendMsgHandler.java
@@ -1,9 +1,6 @@
 package com.las.strategy.handle;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.las.strategy.BotMsgHandler;
-import com.las.utils.CmdUtil;
 import com.las.utils.StrUtils;
 import org.apache.log4j.Logger;
 
@@ -13,10 +10,6 @@ public class FriendMsgHandler extends BotMsgHandler {
 
     @Override
     public void exec() {
-        JSONObject sender = getSender();
-        JSONArray msgChain = getMsgChain();
-        logger.info("得到了sender --> " + sender.toJSONString());
-        logger.info("得到了msgChain --> " + msgChain.toJSONString());
         String msgData = getMsgData();
         if (StrUtils.isNotBlank(msgData)) {
             exeCommand(msgData, getUserId(), getId(), getMsgType());
