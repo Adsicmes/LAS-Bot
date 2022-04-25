c93e7d6c579dbd6a6507de6b0123039b78eb5d08
dullwolf <2547170055@qq.com>
2022-04-25 23:37:36 +0800

    更新脚本

--- 启动.bat
+++ 启动.bat
@@ -1,4 +1,5 @@
 call mvn clean package -Pbot
 copy .\target\qqbot.jar qqbot.jar
-call java -Djava.ext.dirs=./target/lib -jar qqbot.jar
+CHCP 65001
+call java -Djava.ext.dirs=./target/lib -Dfile.encoding=utf-8 -jar qqbot.jar
 call pause
