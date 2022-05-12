cca47bb5624a268ff34e09dad3e462aea9ca3d5e
赵立田 <o_zhaolitian@cvte.com>
2022-05-12 19:13:45 +0800

    update

--- src/main/java/com/las/utils/WxCmdUtil.java
+++ src/main/java/com/las/utils/WxCmdUtil.java
@@ -1,7 +1,47 @@
 package com.las.utils;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.las.config.AppConfigs;
+import com.las.dto.WeChatMsgDTO;
+import org.apache.log4j.Logger;
+
+import java.util.Date;
+
 public class WxCmdUtil {
 
+    private static Logger logger = Logger.getLogger(WxCmdUtil.class);
+
+    private static void sendMsg(String json) {
+        try {
+            AppConfigs.WX_PUSH_SERVER.send(json);
+        } catch (Exception e) {
+            logger.error("发送WX信息失败，原因：" + e.getMessage());
+        }
+    }
+
+    public static void getContact() {
+        String id = String.valueOf(new Date().getTime());
+        WeChatMsgDTO msgDTO = new WeChatMsgDTO();
+        msgDTO.setContent("op:list member");
+        msgDTO.setWxid("null");
+        msgDTO.setType(5010);
+        msgDTO.setId(id);
+        String json = JSONObject.toJSONString(msgDTO);
+        logger.info("微信getContact:" + json);
+        sendMsg(json);
+    }
+
+    public static void sendMsg(String wxid, String text) {
+        String id = String.valueOf(new Date().getTime());
+        WeChatMsgDTO msgDTO = new WeChatMsgDTO();
+        msgDTO.setContent(text);
+        msgDTO.setWxid(wxid);
+        msgDTO.setType(555);
+        msgDTO.setId(id);
+        String json = JSONObject.toJSONString(msgDTO);
+        logger.info("微信sendMsg:" + json);
+        sendMsg(json);
+    }
 
 }
