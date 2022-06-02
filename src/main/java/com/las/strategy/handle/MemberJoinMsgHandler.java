4d6fc753ab0a5f3946e1e6fc4bf136b1ff1dbee9
赵立田 <o_zhaolitian@cvte.com>
2022-06-02 17:48:44 +0800

    欢迎新人入群提示功能完成

--- src/main/java/com/las/strategy/handle/MemberJoinMsgHandler.java
+++ src/main/java/com/las/strategy/handle/MemberJoinMsgHandler.java
@@ -1,8 +1,22 @@
 package com.las.strategy.handle;
 
+import com.las.common.Constant;
+import com.las.model.GroupExt;
+import com.las.utils.CmdUtil;
+import com.las.utils.StrUtils;
+
 public class MemberJoinMsgHandler extends BotMsgHandler {
+
     @Override
     public void exec() {
-
+        if (Constant.MESSAGE_TYPE_GROUP == getMsgType()) {
+            GroupExt groupExt = getGroupExtDao().findByGid(getId());
+            String tip = groupExt.getAttribute1();
+            // 若欢迎提示不为空，则欢迎新人入群
+            if (StrUtils.isNotBlank(tip)) {
+                CmdUtil.sendAtMessage(tip, getUserId(), getUserId(), getId(), getMsgType());
+            }
+        }
     }
+
 }
