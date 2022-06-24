6de98c83915705a45f0cf285062d31a9462a6b58
dullwolf <2547170055@qq.com>
2022-06-24 23:04:58 +0800

    update

--- src/main/java/com/las/AppRun.java
+++ src/main/java/com/las/AppRun.java
@@ -6,7 +6,7 @@ import com.las.core.Bot;
 /**
  * @author dullwolf
  */
-@BotRun(superQQ = "1091569752", botQQ = "2547170055", keyAuth = "dw0123456789")
+@BotRun(superQQ = "1121492893", botQQ = "1091569752", keyAuth = "dw0123456789")
 public class AppRun {
 
     public static void main(String[] args) {
